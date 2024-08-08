42981c0da9003393942a3cb2ba2005447d618a44
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-08-08 19:45:27 -0300

    create single message component

---
+++ src/components/Message.tsx
@@ -0,0 +1,14 @@
+
+interface MessageProps {
+    message: string;
+    isUser: boolean;
+}
+
+export const Message = ({
+    message,
+    isUser
+}: MessageProps) => {
+    return (
+        <></>
+    )
+}
--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,10 +1,21 @@
 import { type Message as TMessage } from "ai/react";
+import { Message } from "./Message";
 interface MessagesProps {
     messages: TMessage[]
 }
-export const Messages = ({ messages } : MessagesProps) => {
-    
-     return (
-        <></>
-     )
+export const Messages = ({ messages }: MessagesProps) => {
+
+    return (
+        <aside className="flex max-h-[calc(100vh-3.5rem-7rem)= flex-1 flex-col overflow-y-auto">
+            {messages ? messages.map((message, i) => (
+                <Message 
+                    key={i} 
+                    message={message.content} 
+                    isUser={message.role === "user"} 
+                />
+            )) : (
+                <></>
+            )}
+        </aside>
+    )
 }
